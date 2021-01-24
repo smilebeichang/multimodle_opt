c6796a17cdcbf7bdd4624be5d3f69cd7ed28a038
smilebeichang <songbeichang@163.com>
2021-01-24 23:41:41 +0800

    junit

--- src/main/java/net/eyun/controller/testMath.java
+++ src/main/java/net/eyun/controller/testMath.java
@@ -12,10 +12,54 @@ public class testMath {
             int i2 = Math.abs(new Random().nextInt()) % 10 + 20;
 
             System.out.println("=== main ==="+i2);
+            int i3 = Math.abs(new Random().nextInt()) % 10 + 20;
+
+            System.out.println("=== main ==="+i2);
+            int i4 = Math.abs(new Random().nextInt()) % 10 + 20;
+
+            System.out.println("=== main ==="+i2);
+            int i5 = Math.abs(new Random().nextInt()) % 10 + 20;
+
+            System.out.println("=== main ==="+i2);
+
+            int i6 = Math.abs(new Random().nextInt()) % 10 + 20;
+
+            System.out.println("=== main ==="+i2);
+            int i7 = Math.abs(new Random().nextInt()) % 10 + 20;
+
+            System.out.println("=== main ==="+i2);
+
+            int i8 = Math.abs(new Random().nextInt()) % 10 + 20;
+
+            System.out.println("=== main ==="+i2);
+            int i9 = Math.abs(new Random().nextInt()) % 10 + 20;
+
+            System.out.println("=== main ==="+i2);
+
+            int i10 = Math.abs(new Random().nextInt()) % 10 + 20;
+
+            System.out.println("=== main ==="+i2);
+            int i11 = Math.abs(new Random().nextInt()) % 10 + 20;
+
+            System.out.println("=== main ==="+i2);
+
+
         }
-
-
     }
+
+    public static void main2(String[] args) {
+        System.out.println("=== main2 ===");
+    }
+
+    public static void main3(String[] args) {
+        System.out.println("=== main2 ===");
+    }
+
+    public static void main4(String[] args) {
+        System.out.println("=== main2 ===");
+    }
+
+
 }
 
 
